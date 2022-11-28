ae6d93fa36a7a94e3ce6065b457f8d95266b94d4
b1shal711 <ba070101@gmail.com>
2022-11-28 14:31:17 +0530

    add gitignore,procfile and update ports

--- app.js
+++ app.js
@@ -126,6 +126,12 @@ app.post("/delete" , function(req,res){
   }
 });
 
-app.listen("3000",function(){
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+
+app.listen(port,function(){
   console.log("server running on port 3000");
 });
